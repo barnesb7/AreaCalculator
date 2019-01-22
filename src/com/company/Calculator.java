fdfada7a71c66943de3551dceaefecff89cbc165
Brian B <brianb@MacBook-Pro-4.local>
2019-01-22 14:29:19 -0500

    Adding ability of calculateAreaOfShape to take into account shape when calculating area

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
     private double width = 5.0;
     private double height = 6.0;
-
+    private String shapeUsedToCalculateArea;
 
     public void setWidth(double userEnteredWidth){
         width = userEnteredWidth;
@@ -23,9 +23,23 @@ public class Calculator {
         return height;
     }
 
-    public double calculateArea(double enteredWidth, double enteredHeight){
-        double area = enteredWidth * enteredHeight;
-        return area;
+    public double calculateAreaOfShape(String shape, double enteredWidth, double enteredHeight){
+
+        shapeUsedToCalculateArea = shape;
+        double area;
+
+
+        if(shapeUsedToCalculateArea == "rectangle"){
+            area = enteredWidth * enteredHeight;
+            return area;
+
+        } else if (shapeUsedToCalculateArea == "triangle") {
+            area = (enteredWidth * enteredHeight)/2;
+            return area;
+        } else {
+            return 0.0;
+        }
+
 
     }
 
--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -5,6 +5,9 @@ public class CalculatorDemo {
     public static void main(String[] args) {
 	Calculator calculator = new Calculator();
 
-	System.out.println("The area is " + calculator.calculateArea(5.0, 6.0) + " meters.");
+	System.out.println("The area of your rectangle is " + calculator.calculateAreaOfShape("rectangle", 5.0, 6.0) + " meters.");
+
+	System.out.println("The area of your triangle is " + calculator.calculateAreaOfShape("triangle", 5.0, 6.0) + " meters.");
+
     }
 }
