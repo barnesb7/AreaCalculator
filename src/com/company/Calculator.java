163566d65393d889156bff0cff8d8833360557a5
Brian B <brianb@MacBook-Pro-4.local>
2019-01-22 14:10:47 -0500

    Adding Calculator class with getter and setter methods

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Calculator {
+
+    private double width = 5.0;
+    private double height = 6.0;
+
+
+    public void setWidth(double userEnteredWidth){
+        width = userEnteredWidth;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+
+    public void setHeight(double userEnteredHeight){
+        height = userEnteredHeight;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+}
